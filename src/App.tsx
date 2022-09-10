07ce91e8c89ed7c4166c894e06c24498c21e543b
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-09-10 11:31:24 -0300

    feat: configurando dependencias

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 function App() {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
